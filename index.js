e5af300d4263a7b82d4d71f73b680f5dc19a0676
Con Sınov <hakanunal_1996@hotmail.com>
2023-08-26 01:16:00 +0300

    updated

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: "https://pdf-generator-pi.vercel.app",
             },
         ],
     },
